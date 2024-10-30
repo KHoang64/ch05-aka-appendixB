bb0a2349a574d0f766b144cddef69f42f776362b
KHoang64 <hkent625@gmail.com>
2024-10-29 22:27:49 -0400

    Testing Apollo setup to diagnose 404 error on /api/graphql endpoint

--- my-app/graphql/schema.ts
+++ my-app/graphql/schema.ts
@@ -1,28 +1,7 @@
-import gql from "graphql-tag";
+import { gql } from 'graphql-tag';
 
 export const typeDefs = gql`
-
-type LocationWeatherType {
-    zip: String!
-    weather: String!
-    tempC: String!
-    tempF: String!
-    friends: [String]!
-}
-
-input LocationWeatherInput {
-    zip: String!
-    weather: String
-    tempC: String
-    tempF: String
-    friends: [String]
-}
-
-type Query {
-    weather(zip: String): [LocationWeatherType]!
-}
-
-type Mutation {
-    weather(data: LocationWeatherInput): [LocationWeatherType]!
-}
+  type Query {
+    hello: String
+  }
 `;
