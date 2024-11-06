33b386c88ab674720271ec6c6a9095075e8e3e92
KHoang64 <hkent625@gmail.com>
2024-11-06 00:33:36 -0500

    exercise 7 Connect the GraphQL API to the Database, probably works with the example:96815

--- my-app/graphql/resolvers.ts
+++ my-app/graphql/resolvers.ts
@@ -1,4 +1,6 @@
+import { findByZip, updateByZip } from "@/mongoose/weather/services";
 import { db } from "./data";
+
 
 declare interface WeatherInterface {
     zip: string;
@@ -11,12 +13,15 @@ declare interface WeatherInterface {
 export const resolvers = {
     Query: {
         weather: async (_: any, param: WeatherInterface) => {
-            return [db.find((item) => item.zip === param.zip)];
-        }
+            let data = await findByZip(param.zip);
+            return [data];
+        },
     },
     Mutation: {
         weather: async (_: any, param: { data: WeatherInterface }) => {
-            return [db.find((item) => item.zip === param.data.zip)];
+            await updateByZip(param.data.zip, param.data);
+            let data = await findByZip(param.data.zip);
+            return [data];
         }
     }
 };
